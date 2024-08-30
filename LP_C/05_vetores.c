bbcb2c404b71c9a3412822299d1b5274d957b075
José Victor <victorgroba2@gmail.com>
2024-08-30 16:53:02 -0300

    30 de agosto

--- LP_C/05_vetores.c
+++ LP_C/05_vetores.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
+#define N 5
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
-   int N, i;
+   int i;
   
    
-   printf("Quantos números voce vai digitar? ");
-   scanf("%d", &N);
+   
 
      double vet [N];
 
    for(i=0;i<N;i++){
-    printf("Digite um número na posição   do vetor: ");
+    printf("Digite um número na posição   do vetor:");
     scanf("%lf", &vet[i]);
 
    }
 
    for(i=0;i<N;i++){
-    printf("Vetor : %lf ", vet[i]);
+    printf(" %.1lf ", vet[i]);
+   
    }
-
     return 0;
 }
