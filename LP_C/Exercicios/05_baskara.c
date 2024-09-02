3445bd10e424302022e82be7915a6390294582dd
José Victor <victorgroba2@gmail.com>
2024-09-02 15:07:07 -0300

    02 de setembro

---
+++ LP_C/Exercicios/05_baskara.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    double a, b, c, delta, x1, x2;
+
+    printf("Digite os coeficientes da equação: (a/b/c)");
+    
+
+    //delta não pode ser negativo
+    //Coeficiente a não pode ser Zero
+    return 0;
+}
